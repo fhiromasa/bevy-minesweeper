5f9d871bfe9907daaedcc0c0d2a8fea051f5194d
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2024-09-23 02:04:37 +0900

    set up aplication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    let window = Window {
+        title: "bevy minesweeper".to_string(),
+        resolution: (700., 800.).into(),
+        resizable: false,
+        ..default()
+    };
+    let primary_window = Some(window);
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window,
+        ..default()
+    }));
+
+    #[cfg(feature = "debug")]
+    app.add_plugins(WorldInspectorPlugin::new());
+
+    app.add_systems(Startup, setup_camera);
+    app.run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
