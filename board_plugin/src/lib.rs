e1a302b2b5885152c3a7c016df50dc0b93a70ef6
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2024-09-23 18:52:44 +0900

    lib.rsの実装

--- board_plugin/src/lib.rs
+++ board_plugin/src/lib.rs
@@ -1,4 +1,26 @@
+use bevy::{app::Plugin, prelude::*};
+use resources::tile_map::TileMap;
+
 pub mod components;
 pub mod resources;
 
 pub struct BoardPlugin;
+
+impl BoardPlugin {
+    fn create_board() {
+        let mut tile_map = TileMap::empty(20, 20);
+        tile_map.set_bomb(40);
+
+        #[cfg(feature = "debug")]
+        log::info!("{}", tile_map.console_output());
+    }
+}
+
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, Self::create_board);
+
+        #[cfg(feature = "debug")]
+        log::info!("Loaded BoardPlugin");
+    }
+}
